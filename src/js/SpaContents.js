2ec5670b0254b145c220d8e34ed64c27168b81ad
Fahasor <rad.nikita@list.ru>
2024-08-28 19:41:38 +0300

    add basic SPA logic

--- src/js/SpaContents.js
+++ src/js/SpaContents.js
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import AccountsListPage from "./pages/AccountsListPage";
+import AddAccountForm from "./AddAccountForm";
+
 export default function SpaContents() {
-    return(<div></div>);
+    const [currentPage, setCurrentPage] = useState(<AddAccountForm/>);
+    
+    return(
+        <>
+            <div>
+                <button
+                    onClick = {() => setCurrentPage(<AddAccountForm/>)}
+                >
+                    Регистрация
+                </button>
+                <button
+                    onClick = {() => setCurrentPage(<AccountsListPage url="/drivers"/>)}
+                >
+                    Водители
+                </button>
+                <button
+                    onClick = {() => setCurrentPage(<AccountsListPage url="/users"/>)}
+                >
+                    Пользователи
+                </button>
+            </div>
+            {currentPage}
+        </>
+    )
 }
